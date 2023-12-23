c5a9ce69dcc340b53eddc5b1820a83fe94aa3499
rutvalthonge4312 <rutvalthonge4312@gmail.com>
2023-12-23 23:56:06 +0530

    Added Dark Mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,39 @@
 import React from 'react'
 
 function Navbar() {
+    const handleMode = (e) => {
+        if (e.target.checked) {
+            document.body.style.backgroundColor = "rgb(31, 31, 31)";
+
+            var containers = document.getElementsByClassName("container");
+            var card = document.getElementsByClassName("card");
+            var staticBackdrop = document.getElementsByClassName("modal-content");
+            staticBackdrop[0].classList.add("dark")
+            for (var i = 0; i < containers.length; i++) {
+                containers[i].classList.add("dark");
+
+            }
+            for (var i = 0; i < card.length; i++) {
+                card[i].classList.add("darkCard");
+            }
+        }
+        else {
+            document.body.style.backgroundColor = "rgb(231, 231, 231)";
+            var containers = document.getElementsByClassName("container");
+            var card = document.getElementsByClassName("card");
+            var staticBackdrop = document.getElementsByClassName("modal-content");
+            staticBackdrop[0].classList.remove("dark")
+            for (var i = 0; i < containers.length; i++) {
+                containers[i].classList.remove("dark");
+
+            }
+            for (var i = 0; i < card.length; i++) {
+                card[i].classList.remove("darkCard");
+            }
+
+        }
+
+    }
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
@@ -16,25 +49,18 @@ function Navbar() {
                         <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
+
+
                     </ul>
+                    <div class="form-check form-switch mx-3">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={handleMode} />
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Dark Mode</label>
+                    </div>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
+
                 </div>
             </div>
         </nav>
