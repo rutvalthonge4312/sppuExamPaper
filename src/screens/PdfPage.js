03e5e7aed6005ef103bf896be2d0be209bebad38
rutvalthonge4312 <rutvalthonge4312@gmail.com>
2023-12-25 21:56:27 +0530

    Added PDF Screen

---
+++ src/screens/PdfPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+function PdfPage() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <div className='homePage'>
+
+            </div>
+        </>
+    )
+}
+
+export default PdfPage
